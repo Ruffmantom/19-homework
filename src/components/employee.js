01cad844f24eed1c8c36271508421f3941008963
Tom Ruff <ruffmantom@gmail.com>
2020-04-10 10:39:38 -0600

    added code for displaying hello world!

---
+++ src/components/employee.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Employee() {
+    return (
+        <div>
+            <p>
+                hello world!
+            </p>
+        </div>
+    )
+}
+
+export default Employee;
